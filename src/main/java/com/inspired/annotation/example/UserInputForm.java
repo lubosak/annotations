a3c591b1c5eee2c4cd2f270deed3e65f8e5feb62
Lubos Bosak <lubos.bosak@gmail.com>
2019-02-17 22:23:18 +0100

    UserInputForm plain impl

---
+++ src/main/java/com/inspired/annotation/example/UserInputForm.java
@@ -0,0 +1,37 @@
+package com.inspired.annotation.example;
+
+import java.time.LocalDate;
+
+public class UserInputForm {
+
+	private LocalDate birthDate;
+	
+	private LocalDate effectiveDate;
+	
+	private LocalDate expirationDate;
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public LocalDate getEffectiveDate() {
+		return effectiveDate;
+	}
+
+	public void setEffectiveDate(LocalDate effectiveDate) {
+		this.effectiveDate = effectiveDate;
+	}
+
+	public LocalDate getExpirationDate() {
+		return expirationDate;
+	}
+
+	public void setExpirationDate(LocalDate expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+	
+}
